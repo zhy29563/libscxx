b0f8b7dbe714f31bd2b9f9deddfc5442bb678040
Steven Zhou <zhy29563@hotmail.com>
2024-08-07 14:21:24 +0000

    vector pod

--- sources/single_std_vector.cpp
+++ sources/single_std_vector.cpp
@@ -1,18 +1,22 @@
+#include <algorithm>
+#include <functional>
 #include <iomanip>
 #include <iostream>
+#include <string>
 #include <type_traits>
 #include <typeinfo>
 #include <vector>
 
 template <class T>
-static void PrintVector(const std::vector<T>& vec) {
+static void PrintVector(const std::vector<T>& vec, const std::string& description) {
   std::cout << "=========================================================================================" << std::endl;
-  std::cout << "class name: " << typeid(std::vector<int>).name() << std::endl;
-  std::cout << "size      : " << vec.size() << std::endl;
-  std::cout << "empty     : " << vec.empty() << std::endl;
-  std::cout << "capacity  : " << vec.capacity() << std::endl;
-  std::cout << "max_size  : " << vec.max_size() << std::endl;
-  std::cout << "data      : " << vec.data() << std::endl;
+  std::cout << "description: " << description << std::endl;
+  std::cout << "class name : " << typeid(std::vector<int>).name() << std::endl;
+  std::cout << "size       : " << vec.size() << std::endl;
+  std::cout << "empty      : " << vec.empty() << std::endl;
+  std::cout << "capacity   : " << vec.capacity() << std::endl;
+  std::cout << "max_size   : " << vec.max_size() << std::endl;
+  std::cout << "data       : " << vec.data() << std::endl;
 
   for (std::size_t index = 0; index < vec.size(); index++) {
     std::cout << "Index: " << std::setw(3) << std::setfill('0') << index << ", Value: " << vec[index] << std::endl;
@@ -20,32 +24,58 @@ static void PrintVector(const std::vector<T>& vec) {
 }
 
 int main() {
-  // 创建
-  std::vector<int> vec_int;
-  PrintVector(vec_int);
+  {
+    std::vector<int> vec_pod_one;
+    PrintVector(vec_pod_one, "create vec_pod_one");
 
-  // 修改尺寸
-  vec_int.resize(10);
-  PrintVector(vec_int);
+    vec_pod_one.resize(5);
+    PrintVector(vec_pod_one, "resize vec_pod_one");
 
-  // 增
-  vec_int.push_back(10);
-  vec_int.emplace_back(20);
-  vec_int.insert(vec_int.begin(), 30);
-  PrintVector(vec_int);
+    std::vector<int> vec_pod_two(5);
+    PrintVector(vec_pod_two, "create vec_pod_two");
+  }
 
-  int a = 100;
-  vec_int.emplace_back(a);
-  a                           = 200;
-  vec_int[vec_int.size() - 1] = 400;
-  std::cout << "a = " << a << std::endl;
-  PrintVector(vec_int);
+  {
+    std::vector<int> vec_pod;
+    vec_pod.emplace_back(20);
+    vec_pod.emplace_back(30);
+    vec_pod.emplace_back(40);
+    PrintVector(vec_pod, "pod emplace_back");
 
-  // 改
+    vec_pod.insert(vec_pod.begin(), 50);
+    PrintVector(vec_pod, "pod insert");
+  }
 
-  // 查
+  {
+    int              a = 10;
+    std::vector<int> vec_pod;
+    vec_pod.emplace_back(a);
+    a                           = 200;
+    vec_pod[vec_pod.size() - 1] = 400;
+    std::cout << "a = " << a << std::endl;
+    PrintVector(vec_pod, "modify pod");
+  }
 
-  // 删
+  {
+    std::vector<int> vec_pod{1, 4, 6, 2, 7, 4};
+    PrintVector(vec_pod, "before sort");
+
+    std::sort(vec_pod.begin(), vec_pod.end());
+    PrintVector(vec_pod, "after sort");
+  }
+
+  {
+    std::vector<int> vec_pod{1, 4, 6, 2, 7, 4};
+    PrintVector(vec_pod, "before find");
+
+    auto iter = std::find(vec_pod.begin(), vec_pod.end(), 4);
+    if (iter == vec_pod.end()) {
+      std::cout << "find is failed." << std::endl;
+    } else {
+      std::ptrdiff_t distance = std::distance(vec_pod.begin(), iter);
+      std::cout << "distance is: " << distance << std::endl;
+    }
+  }
 
   return EXIT_SUCCESS;
 }
